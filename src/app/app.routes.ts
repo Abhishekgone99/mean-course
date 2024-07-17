fa1b117eea277a09e49fa6707a30f387e3626e04
Abhishek gone <abhishekgone99@gmail.com>
2024-07-17 12:49:32 +0530

    adding route guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,17 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-    // {path:'', redirectTo:'list', pathMatch:'full'},
-    {path:'', component : PostListComponent},
-    {path:'create', component : PostCreateComponent},
-    {path:"edit/:postId", component: PostCreateComponent},
-    {path: "login", component: LoginComponent},
-    {path: "signup", component: SignupComponent}
-]; 
+  // {path:'', redirectTo:'list', pathMatch:'full'},
+  { path: '', component: PostListComponent },
+  { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
+  {
+    path: 'edit/:postId',
+    component: PostCreateComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+];
