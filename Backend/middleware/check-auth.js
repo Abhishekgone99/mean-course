770cc7e064e0ed33982b16507a3cd6244cd438dc
Abhishek gone <abhishekgone99@gmail.com>
2024-07-17 15:42:08 +0530

    adding the userId to posts

--- Backend/middleware/check-auth.js
+++ Backend/middleware/check-auth.js
@@ -3,7 +3,11 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "secret_key_that_should_be_longer_for_better_token");
+    const decodedToken = jwt.verify(
+      token,
+      "secret_key_that_should_be_longer_for_better_token"
+    );
+    req.userData = { email: decodedToken.email, userId: decodedToken.userId };
     next();
   } catch (error) {
     res.status(401).json({
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,78 +4,100 @@ import { Subject, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
-   private posts:Ipost[] = [];
-   private postsUpdated = new Subject<{posts: Ipost[], postCount: number}>()
+  private posts: Ipost[] = [];
+  private postsUpdated = new Subject<{ posts: Ipost[]; postCount: number }>();
 
-  constructor(private http:HttpClient, private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  getPosts(postsPerPage:number,currentPage:number){
-    const querParams = `?pagesize=${postsPerPage}&page=${currentPage}`
-    this.http.get<{message:string, posts:any,maxPosts:number}>('http://localhost:3000/api/posts' + querParams)
-    .pipe(map(postData =>{
-      return {posts: postData.posts.map((post) =>{
-        return {
-          title: post.title,
-          content:post.content,
-          id:post._id,
-          imagePath: post.imagePath
-        }
-      }), maxPosts : postData.maxPosts
-    }
-    }))
-    .subscribe((transformedPostsData)=>{
-      this.posts = transformedPostsData.posts;
-      this.postsUpdated.next({posts: [...this.posts], postCount: transformedPostsData.maxPosts })
-    })
+  getPosts(postsPerPage: number, currentPage: number) {
+    const querParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
+    this.http
+      .get<{ message: string; posts: any; maxPosts: number }>(
+        'http://localhost:3000/api/posts' + querParams
+      )
+      .pipe(
+        map((postData) => {
+          return {
+            posts: postData.posts.map((post) => {
+              return {
+                title: post.title,
+                content: post.content,
+                id: post._id,
+                imagePath: post.imagePath,
+                creator: post.creator,
+              };
+            }),
+            maxPosts: postData.maxPosts,
+          };
+        })
+      )
+      .subscribe((transformedPostsData) => {
+        this.posts = transformedPostsData.posts;
+        this.postsUpdated.next({
+          posts: [...this.posts],
+          postCount: transformedPostsData.maxPosts,
+        });
+      });
   }
 
-  getPost(id: string){
+  getPost(id: string) {
     // return {...this.posts.find(p => p.id === id)};
-    return  this.http.get<{_id:string,title:string,content:string,imagePath:string}>('http://localhost:3000/api/posts/' + id)
+    return this.http.get<{
+      _id: string;
+      title: string;
+      content: string;
+      imagePath: string;
+    }>('http://localhost:3000/api/posts/' + id);
   }
 
-  getPostUpdateListener(){
+  getPostUpdateListener() {
     return this.postsUpdated.asObservable();
   }
 
-  addPosts(title: string, content: string , image: File){
-      const postData = new FormData();
-      postData.append('title',title);
-      postData.append('content',content);
-      postData.append('image',image, title)
-      this.http.post<{message:string,post: Ipost}>('http://localhost:3000/api/posts',postData).subscribe((responseData)=>{
+  addPosts(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
+    this.http
+      .post<{ message: string; post: Ipost }>(
+        'http://localhost:3000/api/posts',
+        postData
+      )
+      .subscribe((responseData) => {
         this.router.navigate(['/']);
-      })
+      });
   }
 
-  updatePost(id:string, title:string, content:string, image: any){
+  updatePost(id: string, title: string, content: string, image: any) {
     let postData: Ipost | FormData;
-    if(typeof image === 'object'){
+    if (typeof image === 'object') {
       postData = new FormData();
-      postData.append("id", id);
-      postData.append("title", title);
-      postData.append("content", content);
-      postData.append("image", image, title);
-    }else{
+      postData.append('id', id);
+      postData.append('title', title);
+      postData.append('content', content);
+      postData.append('image', image, title);
+    } else {
       postData = {
-        id : id,
+        id: id,
         title: title,
-        content : content,
-        imagePath : image
-      } 
-    };
+        content: content,
+        imagePath: image,
+      };
+    }
 
-    this.http.put('http://localhost:3000/api/posts/' + id, postData).subscribe((response)=>{
-    this.router.navigate(["/"])
-    })
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, postData)
+      .subscribe((response) => {
+        this.router.navigate(['/']);
+      });
   }
 
-  deletePost(postId: any){
-     return  this.http.delete('http://localhost:3000/api/posts/'+ postId);
+  deletePost(postId: any) {
+    return this.http.delete('http://localhost:3000/api/posts/' + postId);
   }
 }
